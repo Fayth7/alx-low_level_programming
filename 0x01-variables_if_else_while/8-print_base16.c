525355f82c91199ff7ee7987df3e579804afcad8
Fayth7 <flexflakes@gmail.com>
2023-02-17 09:22:36 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlic.h>
+#include <time.h>
+
+/**
+*main - prints all the numbers of base 16 in lowercase
+*Return: Always 0 (success)
+*/
+
+int main(void)
+
+{
+
+ind d;
+char lc;
+for (d = '0' d <= '9'; d++)
+putchar(lc);
+putchar('\n');
+
+return (0);
+
+}
