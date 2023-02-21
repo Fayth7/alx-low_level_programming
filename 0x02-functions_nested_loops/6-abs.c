23ba994c5b122c4bdbd4c14acadd28fd73473fc6
Fayth7 <flexflakes@gmail.com>
2023-02-21 10:57:45 -0800

    last digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 
 /**
 * _abs - computes the absolute value of an integer
-* @int: int to be checked
+* @n: int to be checked
 * Return: the abs of the int
 */
 
-int _abs(int)
+int _abs(n)
 {
 if (n >= 0)
 return (n);
