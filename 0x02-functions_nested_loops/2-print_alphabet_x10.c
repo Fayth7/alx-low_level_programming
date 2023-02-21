9348d91626c33fefc7ec5e55909496df58ab4638
Fayth7 <flexflakes@gmail.com>
2023-02-21 04:23:50 -0800

    lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- *void print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- *Returns: 0
- */
+*void print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+*Returns: 0
+*/
 
 void print_alphabet_x10(void)
 {
 int count = 0
 char letter;
+ 
 while (count++ <= 9)
-for (letter = 'a'; letter <= 'z'; leteer++)
+for (letter = 'a'; letter <= 'z'; letter++)
 
 _putchar(letter);
-_putchar('\n')
-return (0)
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower - checks if a char is lowercase
+*@c: char to be checked
+*Returns: 1 if c is lowercase otherwise 0
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
