6f7d0c804fc503dc3efa2f150e7bda8b521867e6
Fayth7 <flexflakes@gmail.com>
2023-02-23 09:26:21 -0800

    print square

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,18 @@
 void more_numbers(void)
 {
 int x, y;
-{
-for (x = 1; x <= 10; x++)
+
+for (x = 0; x < 10; x++)
 {
 for (y = 0; y < 15; y++)
 {
 if (y >= 10)
 {
-_putchar('1');
+_putchar((y / 10) + '0');
 }
 _putchar((y % 10) + '0');
 }
 _putchar('\n');
 }
 }
-}
+
