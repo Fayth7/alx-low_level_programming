8cc77f7b795ab90c442234a0cc91155851c728f1
Fayth7 <flexflakes@gmail.com>
2023-02-23 06:39:59 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: Char to be checked
+* Return: 1 if c is uppercase otherwise 0
+int _isupper(int c)
+{
+if (c >= 'A' && C <= 'Z')
+return (1);
+else
+return (0);
+}
