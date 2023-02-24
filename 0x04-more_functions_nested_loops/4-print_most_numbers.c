10412323f0929e6ac6f0fc9b4bb7e1bdce20f93f
Fayth7 <flexflakes@gmail.com>
2023-02-24 01:15:29 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,24 @@
 
 /**
 * print_most_numbers - prints 01356789/n
-*Returns: void
+* Returns: void
 */
+
 void print_most_numbers(void)
 {
-int i;
+int i = 0;
 
-for (i = 0; i < 10; i++)
-(i != 2 && i != 4) ? _putchar(i + '0') : 0; /* ternary operator */
+for (; i < = 9; i++)
+{
+if (i == 2 || i ==4)
+{
+continue;
+}
+else
+{
+_putchar(i + '0');
+}
+}
 _putchar('\n');
 }
 
