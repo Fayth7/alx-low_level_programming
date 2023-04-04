2469ef205b8980e347641d93d09f43d2c8ac1d06
Fayth7 <flexflakes@gmail.com>
2023-04-04 01:47:12 -0700

    listint 2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,18 +3,17 @@
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t 
- *
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-size_t num = 0;
+const listint_t *current = h;
+size_t count = 0;
 
- while (h)
- {
- num++;
- h = h->next;
- }
-
- return (num);
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+return (count);
 }
