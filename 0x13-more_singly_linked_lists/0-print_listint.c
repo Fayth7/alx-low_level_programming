a2ecdae5667d95417ebf0e94c8ec6c90ff3a0876
Fayth7 <flexflakes@gmail.com>
2023-04-04 00:42:23 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+/**
+ * print_listint -  prints all the elements of a listint_t list
+ * @h - linked list to print
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+
+return (count);
+}
