19c8a89167ee6199f0e8ce7592fb0b2c637d6838
Fayth7 <flexflakes@gmail.com>
2023-04-08 01:41:50 -0700

    print binary update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,26 @@
+#include "main.h"
+
 /**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int mask = 1;
 int i, count = 0;
+unsigned long int current;
 
-while (n > mask)
-mask = (mask << 1) | 1;
-
-for (i = 0; mask != 0; i++)
+for (i = 63; i >= 0; i--)
 {
-if (n & mask)
+current = n >> i;
+
+if (current & 1)
 {
 _putchar('1');
 count++;
 }
 else if (count)
 _putchar('0');
- 
-mask = mask >> 1;
 }
-
 if (!count)
 _putchar('0');
 }
