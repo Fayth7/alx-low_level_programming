45f07e3390de0f551a3a859151c3fad548d6ad7e
Fayth7 <flexflakes@gmail.com>
2023-04-10 01:58:47 -0700

    create file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
@@ -10,46 +9,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd, n, w;
-
-    if (!filename)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return (0);
-
-    buf = malloc(sizeof(char) * letters);
-    if (!buf)
-    {
-        close(fd);
-        return (0);
-    }
-
-    n = read(fd, buf, letters);
-    if (n < 0)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    w = 0;
-    while (w < n)
-    {
-        ssize_t m = write(STDOUT_FILENO, buf + w, n - w);
-        if (m < 0)
-        {
-            free(buf);
-            close(fd);
-            return (0);
-        }
-        w += m;
-    }
-
-    free(buf);
-    close(fd);
-    return (w);
+char *buf;
+ssize_t fd, n, w;
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
+buf = malloc(sizeof(char) * letters);
+if (!buf)
+{
+close(fd);
+return (0);
+}
+n = read(fd, buf, letters);
+if (n < 0)
+{
+free(buf);
+close(fd);
+return (0);
+}
+w = 0;
+while (w < n)
+{
+ssize_t m = write(STDOUT_FILENO, buf + w, n - w);
+if (m < 0)
+{
+free(buf);
+close(fd);
+return (0);
+}
+w += m;
+}
+free(buf);
+close(fd);
+return (w);
 }
 
