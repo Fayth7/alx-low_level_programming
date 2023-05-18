eeee743c9142b30566940fae4c2105b1745b16d5
Fayth7 <flexflakes@gmail.com>
2023-05-18 04:07:34 -0700

    add node

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list.
+ * @h: Pointer to the head of the doubly linked list.
+ * Returns: The number of elements in the linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *current = h;
+size_t count = 0;
+
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+
+return (count);
+}
