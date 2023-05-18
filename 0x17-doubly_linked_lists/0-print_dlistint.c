0fe202c474e717dd595ef1791322e40a1b452916
Fayth7 <flexflakes@gmail.com>
2023-05-18 03:39:27 -0700

    listss

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,24 +4,20 @@
 #include "lists.h"
 
 /**
- * dlistint_t - prints all the elements of a list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * 
+ * print_dlistint - prints all the elements of a list
+ * @h: name of the pointer
+ * Return: Count
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+const dlistint_t *current = h;
+size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
-
-   
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+count++;
+}
+return (count);
+}
