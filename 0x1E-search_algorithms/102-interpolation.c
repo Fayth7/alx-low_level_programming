22be541b5e56d3db32d07846e0d6ddb5b295f568
Fayth7 <flexflakes@gmail.com>
2023-09-12 04:29:57 -0700

    task 9

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Searches for a value in a sorted array using
+ *                        interpolation search.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The first index where 'value' is located, or -1 if not found or if
+ * 'array' is NULL.
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+size_t low = 0, high = size - 1, pos;
+double f;
+
+if (array == NULL)
+return (-1);
+
+while (low <= high)
+{
+f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+pos = (size_t)(low + f);
+
+printf("Value checked array[%lu] = ", pos);
+
+if (pos >= size)
+{
+printf("out of range\n");
+break;
+}
+else
+{
+printf("[%d]\n", array[pos]);
+}
+
+if (array[pos] == value)
+return ((int)pos);
+
+if (array[pos] < value)
+low = pos + 1;
+else
+high = pos - 1;
+
+if (low == high)
+break;
+}
+return (-1);
+}
