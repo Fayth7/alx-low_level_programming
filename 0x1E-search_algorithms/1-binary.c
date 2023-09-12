774ed925b6f718891e84c7297826ea5cd190a65e
Fayth7 <flexflakes@gmail.com>
2023-09-12 04:15:59 -0700

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,16 +12,16 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+int left, right, mid, i;
 if (array == NULL)
 return (-1);
 
-int left = 0;
-int right = (int)size - 1;
+left = 0;
+right = (int)size - 1;
 
-while (left <= right)
-{
 printf("Searching in array: ");
-for (int i = left; i <= right; i++)
+
+for (i = left; i <= right; i++)
 {
 printf("%d", array[i]);
 if (i < right)
@@ -30,7 +30,9 @@ else
 printf("\n");
 }
 
-int mid = left + (right - left) / 2;
+while (left <= right)
+{
+mid = left + (right - left) / 2;
 
 if (array[mid] == value)
 return (mid);
@@ -38,7 +40,16 @@ else if (array[mid] < value)
 left = mid + 1;
 else
 right = mid - 1;
-}
 
+printf("Searching in array: ");
+for (i = left; i <= right; i++)
+{
+printf("%d", array[i]);
+if (i < right)
+printf(", ");
+else
+printf("\n");
+}
+}
 return (-1);
 }
